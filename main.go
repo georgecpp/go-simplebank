132a9b31b77923a585309ba7322d4115b65aeb2c
George Andronache <georgeandronache.cpp@gmail.com>
2023-08-21 22:27:08 +0200

    embed static frontend files inside golang backend binary (#16)

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 	"net"
 	"net/http"
 
+	_ "github.com/georgecpp/go-simplebank/docs/statik"
 	db "github.com/georgecpp/go-simplebank/db/sqlc"
 	"github.com/georgecpp/go-simplebank/gapi"
 	"github.com/georgecpp/go-simplebank/pb"
 	"github.com/georgecpp/go-simplebank/util"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -95,8 +97,13 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
-	fs := http.FileServer(http.Dir("./docs/swagger"))
-	mux.Handle("/swagger/", http.StripPrefix("/swagger", fs))
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot create statik fs:", err)
+	}
+
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandler)
 
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
