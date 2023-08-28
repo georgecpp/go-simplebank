44d4b7a76f90f6c9b0165d4d783ed3cd228d2b13
George Andronache <georgeandronache.cpp@gmail.com>
2023-08-28 22:53:19 +0200

    Write structured logs for gRPC APIs (#22)

--- util/config.go
+++ util/config.go
@@ -9,6 +9,7 @@ import (
 // Config stores all configurations of the app
 // the values are read by viper from a config file or env variables
 type Config struct {
+	Environment     string  `mapstructure:"ENVIRONMENT"`
 	DBDriver		string	`mapstructure:"DB_DRIVER"`
 	DBSource		string	`mapstructure:"DB_SOURCE"`
 	MigrationURL		string	`mapstructure:"MIGRATION_URL"`
